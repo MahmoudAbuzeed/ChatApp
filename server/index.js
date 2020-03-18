d12444290c7e0979192e64a069163a5778f052e2
Mahmoud Abuzeed <mahmoudabozeed56@gmail.com>
2020-03-18 18:25:09 +0200

    Full application

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const io = socketio(server);
 
 app.use(cors());
 app.use(router);
-
+ 
 io.on('connect', (socket) => {
   socket.on('join', ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
